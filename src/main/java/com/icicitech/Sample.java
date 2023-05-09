0616f92ab16223c4258baf115336f5f9e8c6c37d
Ravikumar <r-swathi.ravikumar@capgemini.com>
2023-05-09 20:46:18 +0530

    a

---
+++ src/main/java/com/icicitech/Sample.java
@@ -0,0 +1,13 @@
+package com.icicitech;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sample 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
